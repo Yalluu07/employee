991c712abaed746e66d96380deb94297a5d378ac
unknown <Administrator@Newcaed71.MDServer.bvb.edu>
2024-03-18 15:13:40 +0530

    made changes

---
+++ employee.java
@@ -0,0 +1,20 @@
+public class employee{
+
+    String Empname="Akash";
+    Double Salary =(double) 80000;
+    String Emp_id="01fe155";
+
+
+public void displaydetails(){
+    System.out.print("The name of employee is"+Empname);
+    System.out.print("The salary of employee is"+Salary);
+    System.out.print("The Emp id of employee is"+Emp_id);
+
+  }
+
+
+public void totalsalary() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'totalsalary'");
+}
+}
